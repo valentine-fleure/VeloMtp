bf704ef94209938abc4d0506de2514ecd133dabe
Lise Viollat <lise.viollat@cefe.cnrs.fr>
2022-12-01 23:23:04 +0100

    continue plot

--- analyses/2_plot.R
+++ analyses/2_plot.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 Course_velo <- read.csv2(here::here(file.path("data","derived-data", "Course_velo.csv")), sep="")
 Fusion <- read.csv2(here::here(file.path("data","derived-data", "Fusion.csv")), sep="")
 
@@ -10,16 +12,24 @@ compt_depstation <- compt_depstation[compt_depstation$Departure.station<59,]
 names(compt_depstation)<-c("numero", "n", "percent")
 Depstation<-merge(Fusion,compt_depstation,by="numero", all=TRUE)
 
+order_dep=Depstation %>% arrange(desc(n))  # arrangement selon n
+order_numero=Depstation %>% arrange(numero)
 
-#### table 
+names(Course_velo)=c("new_account" , "Departure", "Return", "Bike", "Departure.station" ,      
+                     "Return.station", "Covered.distance", "Duration", "Temperature")
+
+Course_velo$Departure<-as.Date(Course_velo$Departure)
+Course_velo2<-Course_velo[Course_velo$Departure<="2022-01-31",]  #seulement mois de janvier
+
+#### table - les 10 premiers sites les plus utilisés
 library(knitr)
-Dep_tab=Depstation[c("Nom", "n")]
+Dep_tab=order_dep[1:10,c("Nom", "n")]
 kable(Dep_tab, caption="Nombre de velos au depart de chaque Station")
 
-### plot 
+### plot graphique 
 library(ggplot2)
 library(viridis)
-library(tidyverse)
+
 
 hist=ggplot(order_dep, aes(n, stats::reorder(Nom,n), fill=n)) + 
   scale_fill_viridis_c()+
@@ -30,31 +40,82 @@ hist=ggplot(order_dep, aes(n, stats::reorder(Nom,n), fill=n)) +
   theme(axis.text.x = element_text(), legend.position = "none")
 
 plotly::ggplotly(hist)
-                 
-order_dep=Depstation %>% arrange(desc(n))        
+              
+hist=ggplot(order_dep, aes(n, stats::reorder(Nom,n), fill=n)) + 
+  scale_fill_viridis_c()+
+  geom_bar(stat="identity") +
+  theme_minimal()+
+  labs(title = 'Nombre de vélos selon les stations',
+       x = 'Nombre de vélos', y = "Stations") +
+  theme(axis.text.x = element_text(), legend.position = "none")
 
+plotly::ggplotly(hist)   
+      
 #### heatmap
-tabCont=table(Course_velo$Departure.station, Course_velo$Return.station)
-tabCont
-corrplot::cor(as.data.frame(tabCont))
-corrplot::corrplot(, method="circle", 
-                   type="upper", order="hclust", tl.col="black", tl.srt=45)
+tabCont=as.data.frame.matrix(table(Course_velo$Departure.station, Course_velo$Return.station))
+tabCont=tabCont[1:58, 1:58]
+tabCont=tabCont[-57, -57]
+
+names(tabCont)=order_numero$Nom
+rownames(tabCont)=order_numero$Nom
+
+library(heatmaply)
+heatmaply(as.matrix(tabCont), 
+               dendrogram = "none",
+               xlab = "", ylab = "", 
+               main = "",
+               scale = "column",
+               margins = c(60,100,40,20),
+               grid_color = "white",
+               grid_width = 0.00001,
+               titleX = FALSE,
+               hide_colorbar = TRUE,
+               branches_lwd = 0.1,
+               label_names = c("Station de depart ", "Station d'arrivé ", "Value"),
+               fontsize_row = 5, fontsize_col = 5,
+               labCol = colnames(as.matrix(tabCont)),
+               labRow = rownames(as.matrix(tabCont)),
+               heatmap_layers = theme(axis.line=element_blank()))
 
 
+#leaflet  --> faire fonction pour arrivé
+Depstation$x=as.numeric(Depstation$x)
+Depstation$y=as.numeric(Depstation$y)
+mean(Depstation$x)
 
+library(leaflet)
+pal <- colorNumeric(palette = "Spectral",
+                    domain = Depstation$n)
 
-
-pal <- colorNumeric(palette = "RdPu",
-                    domain = data$YearSB)
-
-leaflet(points) %>%
+leaflet(Depstation) %>%
   addTiles() %>%
-  setView(lng = 4.387285, lat = 43.58985, zoom = 8)%>%
-  addCircleMarkers(lng = ~longitudes, lat = ~latitudes, weight = 1,
-                   radius = 7, popup = ~labels,
-                   color = ~pal(data$YearSB), fillOpacity=1)%>%
-  addLegend("topleft", pal = pal, values = ~data$YearSB,
+  setView(lng = mean(Depstation$y), lat = mean(Depstation$x), zoom = 15)%>%
+  addCircleMarkers(lng = ~y, lat = ~x, weight = 1,
+                   radius = 7, popup = ~Nom,
+                   color = ~pal(Depstation$n), fillOpacity=10)%>%
+  addLegend("topleft", pal = pal, values = ~Depstation$n,
             title = "Year SB",
             opacity = 1)%>%
-  addLabelOnlyMarkers(~data$X, ~data$Y, label =  ~as.character(data$Site), 
+  addLabelOnlyMarkers(~Depstation$x, ~Depstation$y, label =  ~as.character(Depstation$Nom), 
                       labelOptions = labelOptions(noHide = T, direction = 'top', textOnly = T))
+
+# trop moche --> trop de site
+library(ggridges)
+library(hrbrthemes)
+Course_velo2$Departure.station=as.factor(Course_velo2$Departure.station)
+
+ggplot(Course_velo2, aes(x = Covered.distance, y = Departure.station)) +
+  geom_density_ridges()
+  
+  
+  
+  
+
+  
+  library(ggraph)
+  ggraph(Course_velo2, layout = 'dendrogram', circular = TRUE) + 
+    geom_conn_bundle(data = get_con(from = Course_velo2$Departure.station, to = Course_velo2$Return.station), alpha=0.2, colour="skyblue", tension = .5) + 
+    geom_node_point(aes(filter = leaf, x = x*1.05, y=y*1.05)) +
+    theme_void()
+
+
---
+++ analyses/3_kernel.R
@@ -0,0 +1,46 @@
+#domaine vitale 
+Course_velo <- read.csv2(here::here(file.path("data","derived-data", "Course_velo.csv")), sep="")
+Course_velo2<-Course_velo[Course_velo$Departure<="2022-01-31",] 
+
+kernel=kernelUD(SpatialPoints(data[,c("x","y")]),h="href", grid=300)
+
+pourcent=c(99,95,90,85,75,50,25)
+Kernel_df=data.frame()
+KDAREAS=data.frame()
+
+for (i in 1:length(pourcent)) {
+  pour=pourcent[i]
+  
+  kernel.poly <- getverticeshr(kernel, percent = pour)
+  
+  kdareas=fortify(kernel.poly)
+  kdareas$id=pour
+  
+  res=c(pour,kernel.poly$area)
+  Kernel_df=rbind(Kernel_df,res)
+  KDAREAS=rbind(KDAREAS,kdareas)
+}
+
+### surface kernel 
+names(Kernel_df)=c("pourcent", "area")
+Kernel_df$area=Kernel_df$area*1e8     # faire *1e8 pour avoir en km2 
+print(Kernel_df$area)
+
+KDAREAS$id=as.factor(KDAREAS$id)    
+summary(KDAREAS)
+
+### Plot kernel 
+color=viridis(6)
+ggplot(KDAREAS) +
+  geom_point(data=data,aes(x=x, y=y), alpha=0.4)+
+  geom_polygon(data=KDAREAS[KDAREAS$id==99,], aes(x=long, y=lat,  fill=id, colour=id, group=piece), alpha = 0.5)+
+  geom_polygon(data=KDAREAS[KDAREAS$id==95,], aes(x=long, y=lat,  fill=id, colour=id, group=piece), alpha = 0.5)+
+  geom_polygon(data=KDAREAS[KDAREAS$id==90,], aes(x=long, y=lat, fill=id,  colour=id, group=piece), alpha = 0.5)+
+  geom_polygon(data=KDAREAS[KDAREAS$id==85,], aes(x=long, y=lat,  fill=id, colour=id, group=piece), alpha = 0.5)+
+  geom_polygon(data=KDAREAS[KDAREAS$id==75,], aes(x=long, y=lat,  fill=id, colour=id, group=piece), alpha = 0.5)+
+  geom_polygon(data=KDAREAS[KDAREAS$id==50,], aes(x=long, y=lat,  fill=id, colour=id, group=piece), alpha = 0.5)+
+  labs( x = "Longitude", y = "Lattitude",
+        title ="Site 89 [FRP-BA11317]")+  theme(legend.position = "bottom")+
+  scale_fill_manual(name="Kernel %",values = c(color[6],color[5], color[4], color[3], color[2], color[1]))+
+  scale_color_manual(name="Kernel %", values = c(color[6], color[5], color[4], color[3], color[2], color[1]))
+
