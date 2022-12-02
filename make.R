25ba80b3652b565454116c4fe9379045cc33ace3
vfleure <valentine.fleure@gmail.com>
2022-12-02 13:24:41 +0100

    upadate make

--- make.R
+++ make.R
@@ -31,5 +31,6 @@ devtools::load_all(here::here())
 # source(here::here("analyses", "script_X.R"))
 
 source(here::here("analyses","1_prep_data.R"))
-
+source(here::here("analyses", "2_plot.R"))
+source(here::here("analyses", "3_kernel.R"))
 source(here::here("analyses", "4_plot_temperature.R"))
